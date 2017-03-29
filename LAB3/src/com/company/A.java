0f62aafbe9ef8357c7d9b716cb418ef8b52fd5cc
deformer <serg19-03@mail.ru>
2017-03-29 19:43:16 +0300

    LAB3A complete

--- LAB3/src/com/company/A.java
+++ LAB3/src/com/company/A.java
@@ -18,9 +18,15 @@ public class A {
     private BufferedWriter cout,console;
     private Stack<Integer> stack;
 
+    private void printNumbers() throws IOException{
+        for (int i = 0; i < numbers.length ; i++) {
+            cout.write(String.valueOf(numbers[i] + " "));
+        }
+    }
+
     public void readInfo() throws IOException {
-        cout = new BufferedWriter(new FileWriter("cycle.out"));
-        cin = new BufferedReader(new FileReader("cycle.in"));
+        cout = new BufferedWriter(new FileWriter("output.txt "));
+        cin = new BufferedReader(new FileReader("input.txt "));
         console =  new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] s = cin.readLine().split(" ");
@@ -31,6 +37,7 @@ public class A {
         numbers = new int[n];
         stack = new Stack();
         Arrays.fill(colors, 0);
+        Arrays.fill(numbers, 0);
         graph = new ArrayList[n];
 
         for (int i = 0; i < n; ++i) {
@@ -39,25 +46,24 @@ public class A {
 
         for (int i = 1; i <= m; ++i) {
             String str[] = cin.readLine().split(" ");
-            //console.write( str[0]+" "+str[1]+"\n" );
             int x = Integer.parseInt(str[0]);
             int y = Integer.parseInt(str[1]);
             graph[x-1].add(y-1);
+            graph[y-1].add(x-1);
         }
-        //printGraph();
-        for (int i = 0; i < n ; i++) {
-            if(dfs(i)){
-                printCycle();
-                return;
-            }
+
+        for(int i = 0;i < graph.length;i++){
+            numbers[i] = graph[i].size();
         }
-        cout.write("NO");
+
+        printNumbers();
+
         cout.close();
         cin.close();
         console.close();
 
     }
-    void main(){
+    public static void main(String[] args) throws IOException{
         A sample = new A();
         sample.readInfo();
     }
